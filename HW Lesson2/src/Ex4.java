9f622d377c840b101599ca348e4c8798412dfa26
AnKun10 <114561027+AnKun10@users.noreply.github.com>
2022-10-26 13:16:10 +0700

    update

--- HW Lesson2/src/Ex4.java
+++ HW Lesson2/src/Ex4.java
@@ -1,29 +1,16 @@
-import javax.swing.text.DateFormatter;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.TemporalAmount;
-import java.util.Date;
 import java.util.Scanner;
 
 //Bài 4: Nhập vào ngày tháng năm sinh của một người theo định dạng nhất định, tính tuổi của người đó (chỉ quan tâm tới năm, không cần tính chính xác tới ngày và tháng).
 public class Ex4 {
-    public static void main(String[] args) throws ParseException {
-        int age;
-        Date birthday;
-        LocalDate now;
-        String date;
-
-        now = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        String formatnow = now.format(formatter);
+    public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter birthday: ");
-        date = input.nextLine();
-
-
+        System.out.println("Enter your birthday: ");
+        String s1 = input.nextLine();
+        LocalDate birthday = LocalDate.parse(s1, DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+        LocalDate now = LocalDate.now();
+        int age = now.getYear() - birthday.getYear();
+        System.out.println("=> Your Age: "+age);
     }
 }
