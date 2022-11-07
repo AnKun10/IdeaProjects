f48829e161d8e525eaec74d2161baa74cb50eceb
AnKun10 <114561027+AnKun10@users.noreply.github.com>
2022-11-07 18:35:48 +0700

    update

--- HW Lesson5/src/Ex4/Main.java
+++ HW Lesson5/src/Ex4/Main.java
@@ -1,4 +1,22 @@
 package Ex4;
-
+//Bài 4:
+//- Tạo lớp đối tượng điểm trong mặt phẳng tọa độ OXY có tên là MyPoint theo lược đồ dưới đây:
+//MyPoint
+//- x: double Tọa độ x
+//- y: double Tọa độ y
+//+ MyPoint() Tạo đối tượng mặc định
+//+ MyPoint(x: double, y: double) Tạo đối tượng có tọa độ xđ trong tham số
+//+ MyPoint(p: MyPoint) Tạo đối tượng là bản sao của đối tượng trong t.số
+//+ getX(): double Trả về tọa độ X
+//+ getY(): double Trả về tọa độ Y
+//+ distance(secondPoint: MyPoint): double Trả về khoảng cách từ điểm này tới điểm thứ hai
+//+distance(p1: MyPoint, p2: MyPoint): double Trả về khoảng cách giữa hai điểm
+//Viết chương trình tạo n đối tượng điểm MyPoint có tọa độ nhập vào từ bàn phím. Tìm hai điểm có khoảng cách
+//lớn nhất. Đưa ra màn hình tọa độ của hai điểm tìm được và giá trị khoảng cách giữa chúng
 public class Main {
+    public static void main(String[] args) {
+        MyPoint system = new MyPoint(1,1);
+        MyPoint[] points = system.input();
+        system.findTwoFurthestPoints(points);
+    }
 }
--- HW Lesson5/src/Ex4/MyPoint.java
+++ HW Lesson5/src/Ex4/MyPoint.java
@@ -1,4 +1,61 @@
 package Ex4;
 
+import java.util.Scanner;
+
 public class MyPoint {
+    public double x;
+    public double y;
+    public MyPoint(){
+        x = 0;
+        y = 0;
+    }
+    public MyPoint(double x,double y){
+        this.x = x;
+        this.y = y;
+    }
+    public MyPoint(MyPoint p){
+        p.x = this.x;
+        p.y = this.y;
+    }
+    public double getX(){
+        return x;
+    }
+    public double getY(){
+        return y;
+    }
+    public double distance(MyPoint secondPoint){
+        return Math.sqrt(Math.pow(this.x - secondPoint.x, 2)+Math.pow(this.y - secondPoint.y, 2));
+    }
+    public double distance(MyPoint p1, MyPoint p2){
+        return Math.sqrt(Math.pow(p1.x - p2.x, 2)+Math.pow(p1.y - p2.y, 2));
+    }
+
+    public MyPoint[] input(){
+        System.out.print("Number of points to create (except MyPoint) = ");
+        int numb = new Scanner(System.in).nextInt();
+        MyPoint[] points = new MyPoint[numb+1];
+        points[numb].x = this.x;
+        points[numb].y = this.y;
+        for (int i = 0; i < numb; i++) {
+            System.out.print("x"+i+" = ");
+            points[i].x = new Scanner(System.in).nextDouble();
+            System.out.print("y"+i+" = ");
+            points[i].y = new Scanner(System.in).nextDouble();
+        }
+        return points;
+    }
+    public void findTwoFurthestPoints(MyPoint[] points){
+        double max = this.distance(points[0],points[1]);
+        int p1MaxIndex = 0, p2MaxIndex = 1;
+        for (int i = 0; i < points.length; i++) {
+            for (int j = i+1; j < points.length; j++) {
+                if (max < this.distance(points[i],points[j])){
+                    max = this.distance(points[i],points[j]);
+                    p1MaxIndex = i;
+                    p2MaxIndex = j;
+                }
+            }
+        }
+        System.out.println("The coordinate of 2 furthest point are A("+points[p1MaxIndex].x+","+points[p1MaxIndex].y+") & B("+points[p2MaxIndex].x+","+points[p2MaxIndex].y+")");
+    }
 }
